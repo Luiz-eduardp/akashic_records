71df645acd3f57a244b973720ff85ea376144d8c
Eduardo <luizzeduardp@gmail.com>
2025-04-01 17:06:02 -0300

    refactor: reorganize main app structure and add loading screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,8 @@
-import 'package:akashic_records/screens/history/history_screen.dart';
-import 'package:flutter/material.dart';
 import 'package:akashic_records/screens/home_screen.dart';
-import 'package:akashic_records/screens/settings/settings_screen.dart';
 import 'package:akashic_records/screens/plugins/plugins_screen.dart';
+import 'package:akashic_records/screens/settings/settings_screen.dart';
 import 'package:akashic_records/themes/app_themes.dart';
+import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:akashic_records/state/app_state.dart';
 
@@ -24,24 +23,42 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final appState = Provider.of<AppState>(context);
-    final settingsLoaded = appState.settingsLoaded;
 
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'Akashic Records',
       themeMode: appState.themeMode,
       theme: AppThemes.lightTheme(appState.accentColor),
       darkTheme: AppThemes.darkTheme(appState.accentColor),
       home:
-          settingsLoaded
-              ? HomeScreen()
-              : const Scaffold(
-                body: Center(child: CircularProgressIndicator()),
-              ),
+          appState.settingsLoaded ? const HomeScreen() : const LoadingScreen(),
       routes: {
-        '/settings': (context) => SettingsScreen(),
-        '/plugins': (context) => PluginsScreen(),
-        '/history': (context) => HistoryScreen(),
+        '/settings': (context) => const SettingsScreen(),
+        '/plugins': (context) => const PluginsScreen(),
       },
+    );
+  }
+}
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const CircularProgressIndicator(),
+            const SizedBox(height: 16),
+            Text(
+              'Carregando...',
+              style: TextStyle(color: Theme.of(context).hintColor),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
