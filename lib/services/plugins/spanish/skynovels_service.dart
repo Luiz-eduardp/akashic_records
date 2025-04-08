0085252c7943c0c8bd1323cf74114f827eb39dce
Eduardo <luizzeduardp@gmail.com>
2025-04-08 10:51:31 -0300

    refactor(skynovels): improve error handling and data parsing
    - Improved error handling for API requests.
    - Added timeout to API calls.
    - Enhanced data parsing for novels and chapters.
    - Improved type safety in data handling.
    - Simplified code structure for better readability.

--- lib/services/plugins/spanish/skynovels_service.dart
+++ lib/services/plugins/spanish/skynovels_service.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
-import 'package:http/http.dart' as http;
+import 'dart:convert';
+
 import 'package:akashic_records/models/model.dart';
 import 'package:akashic_records/models/plugin_service.dart';
-import 'dart:convert';
 import 'package:html/parser.dart' show parse;
+import 'package:http/http.dart' as http;
 
 class SkyNovels implements PluginService {
   @override
@@ -25,17 +26,19 @@ class SkyNovels implements PluginService {
 
   Future<dynamic> _fetchApi(String url) async {
     try {
-      final response = await http.get(
-        Uri.parse(url),
-        headers: {
-          'User-Agent':
-              'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-          'Referer': baseURL,
-        },
-      );
+      final response = await http
+          .get(
+            Uri.parse(url),
+            headers: {
+              'User-Agent':
+                  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+              'Referer': baseURL,
+            },
+          )
+          .timeout(const Duration(seconds: 10));
 
       if (response.statusCode == 200) {
-        String body = response.body;
+        final body = response.body;
         try {
           return jsonDecode(body);
         } catch (jsonError) {
@@ -52,9 +55,6 @@ class SkyNovels implements PluginService {
             return null;
           }
         }
-      } else if (response.statusCode == 403) {
-        print('Erro 403: Acesso proibido para a URL: $url');
-        return null;
       } else {
         print(
           'Falha ao carregar dados de: $url - Status code: ${response.statusCode}',
@@ -72,29 +72,22 @@ class SkyNovels implements PluginService {
     int pageNo, {
     Map<String, dynamic>? filters,
   }) async {
-    String url = '${apiURL}novels?&q';
-    dynamic apiResult = await _fetchApi(url);
+    final url = '${apiURL}novels?&q';
+    final apiResult = await _fetchApi(url);
 
-    if (apiResult == null) {
-      print("apiResult is null, returning empty list");
+    if (apiResult == null || apiResult is String) {
+      print("apiResult is null or String, returning empty list");
       return [];
     }
 
-    if (apiResult is String) {
-      print("apiResult is String, returning empty list");
-      return [];
-    }
-
-    List<Novel> novels = [];
+    final novels = <Novel>[];
 
     if (apiResult is Map && apiResult['novels'] is List) {
-      List<dynamic> novelList = apiResult['novels'];
-      for (var novelData in novelList) {
-        String title = novelData['nvl_title'];
-        String cover =
-            '${apiURL}get-image/' + novelData['image'] + '/novels/false';
-        String path =
-            'novelas/${novelData['id']}/' + novelData['nvl_name'] + '/';
+      final novelList = apiResult['novels'] as List<dynamic>;
+      for (final novelData in novelList) {
+        final title = novelData['nvl_title'] as String;
+        final cover = '${apiURL}get-image/${novelData['image']}/novels/false';
+        final path = 'novelas/${novelData['id']}/${novelData['nvl_name']}/';
 
         novels.add(
           Novel(
@@ -120,30 +113,14 @@ class SkyNovels implements PluginService {
 
   @override
   Future<Novel> parseNovel(String novelPath) async {
-    List<String> parts = novelPath.split('/');
-    String novelId = parts[1];
+    final parts = novelPath.split('/');
+    final novelId = parts[1];
 
-    String url = '${apiURL}novel/$novelId/reading?&q';
-    dynamic apiResult = await _fetchApi(url);
+    final url = '${apiURL}novel/$novelId/reading?&q';
+    final apiResult = await _fetchApi(url);
 
-    if (apiResult == null) {
-      print("apiResult is null, returning default Novel");
-      return Novel(
-        id: novelPath,
-        title: 'Erro ao carregar',
-        coverImageUrl: defaultCover,
-        description: 'Não foi possível carregar os dados do novel.',
-        genres: [],
-        chapters: [],
-        artist: '',
-        statusString: '',
-        pluginId: id,
-        author: '',
-      );
-    }
-
-    if (apiResult is String) {
-      print("apiResult is String, returning default Novel");
+    if (apiResult == null || apiResult is String) {
+      print("apiResult is null or String, returning default Novel");
       return Novel(
         id: novelPath,
         title: 'Erro ao carregar',
@@ -159,45 +136,47 @@ class SkyNovels implements PluginService {
     }
 
     if (apiResult is Map && apiResult['novel'] is List) {
-      Map<String, dynamic>? novelData =
-          (apiResult['novel'] as List).isNotEmpty
-              ? apiResult['novel'][0]
-              : null;
+      final novelData = (apiResult['novel'] as List)
+          .cast<Map<String, dynamic>?>()
+          .firstWhere((element) => element != null, orElse: () => null);
 
       if (novelData != null) {
-        Novel novel = Novel(
+        final novel = Novel(
           id: novelPath,
-          title: novelData['nvl_title'] ?? 'Untitled',
+          title: novelData['nvl_title'] as String? ?? 'Untitled',
           coverImageUrl:
-              '${apiURL}get-image/' + novelData['image'] + '/novels/false',
-          description: novelData['nvl_content'] ?? '',
+              '${apiURL}get-image/${novelData['image']}/novels/false',
+          description: novelData['nvl_content'] as String? ?? '',
           genres:
               (novelData['genres'] as List<dynamic>?)
-                  ?.map<String>((genre) => genre['genre_name'].toString())
+                  ?.map(
+                    (genre) =>
+                        (genre as Map<String, dynamic>)['genre_name'] as String,
+                  )
                   .toList() ??
               [],
           chapters: [],
           artist: '',
-          statusString: novelData['nvl_status'] ?? '',
-          author: novelData['nvl_writer'] ?? '',
+          statusString: novelData['nvl_status'] as String? ?? '',
+          author: novelData['nvl_writer'] as String? ?? '',
           pluginId: id,
         );
 
-        List<Chapter> chapters = [];
-        List<dynamic>? volumes = (novelData['volumes'] as List<dynamic>?);
+        final chapters = <Chapter>[];
+        final volumes = novelData['volumes'] as List<dynamic>?;
         if (volumes != null) {
-          for (var volume in volumes) {
-            List<dynamic>? chapterList = (volume['chapters'] as List<dynamic>?);
+          for (final volume in volumes) {
+            final chapterList = volume['chapters'] as List<dynamic>?;
             if (chapterList != null) {
-              for (var chapter in chapterList) {
-                String chapterPath =
-                    '$novelPath${chapter['id']}/' + chapter['chp_name'];
+              for (final chapter in chapterList) {
+                final chapterPath =
+                    '$novelPath${chapter['id']}/${chapter['chp_name']}';
                 chapters.add(
                   Chapter(
                     id: chapterPath,
-                    title: chapter['chp_index_title'],
+                    title: chapter['chp_index_title'] as String? ?? '',
                     content: '',
-                    order: chapter['chp_index'] ?? 0,
+                    order: chapter['chp_index'] as int? ?? 0,
                   ),
                 );
               }
@@ -209,6 +188,7 @@ class SkyNovels implements PluginService {
         return novel;
       }
     }
+
     print("Unexpected format for novel data, returning default Novel");
     return Novel(
       id: novelPath,
@@ -226,30 +206,24 @@ class SkyNovels implements PluginService {
 
   @override
   Future<String> parseChapter(String chapterPath) async {
-    List<String> parts = chapterPath.split('/');
-    String chapterId = parts[3];
+    final parts = chapterPath.split('/');
+    final chapterId = parts[3];
 
-    String url = '${apiURL}novel-chapter/$chapterId';
-    dynamic apiResult = await _fetchApi(url);
+    final url = '${apiURL}novel-chapter/$chapterId';
+    final apiResult = await _fetchApi(url);
 
-    if (apiResult == null) {
+    if (apiResult == null || apiResult is String) {
       print('apiResult is null for parseChapter');
       return '404';
     }
 
-    if (apiResult is String) {
-      print("apiResult is String for parseChapter, returning 404");
-      return '404';
-    }
-
     if (apiResult is Map && apiResult['chapter'] is List) {
-      Map<String, dynamic>? chapterData =
-          (apiResult['chapter'] as List).isNotEmpty
-              ? apiResult['chapter'][0]
-              : null;
+      final chapterData = (apiResult['chapter'] as List)
+          .cast<Map<String, dynamic>?>()
+          .firstWhere((element) => element != null, orElse: () => null);
 
       if (chapterData != null) {
-        return chapterData['chp_content'] ?? '404';
+        return chapterData['chp_content'] as String? ?? '404';
       }
     }
 
@@ -263,55 +237,48 @@ class SkyNovels implements PluginService {
     int pageNo, {
     Map<String, dynamic>? filters,
   }) async {
-    String searchTermLower = searchTerm.toLowerCase();
-    String url = '${apiURL}novels?&q';
-    dynamic apiResult = await _fetchApi(url);
+    final searchTermLower = searchTerm.toLowerCase();
+    final url = '${apiURL}novels?&q';
+    final apiResult = await _fetchApi(url);
 
-    if (apiResult == null) {
+    if (apiResult == null || apiResult is String) {
       print('apiResult is null for searchNovels');
       return [];
     }
 
-    if (apiResult is String) {
-      print("apiResult is String for searchNovels, returning empty list");
-      return [];
-    }
-
-    List<Novel> novels = [];
+    final novels = <Novel>[];
 
     if (apiResult is Map && apiResult['novels'] is List) {
-      List<dynamic> novelList = apiResult['novels'];
-      List<dynamic>? filteredNovels =
+      final novelList = apiResult['novels'] as List<dynamic>;
+      final filteredNovels =
           novelList
               .where(
-                (novel) => (novel['nvl_title'] as String)
-                    .toLowerCase()
-                    .contains(searchTermLower),
+                (novel) =>
+                    ((novel as Map<String, dynamic>)['nvl_title'] as String)
+                        .toLowerCase()
+                        .contains(searchTermLower),
               )
               .toList();
-      if (filteredNovels != null) {
-        for (var novelData in filteredNovels) {
-          String title = novelData['nvl_title'];
-          String cover =
-              '${apiURL}get-image/' + novelData['image'] + '/novels/false';
-          String path =
-              'novelas/${novelData['id']}/' + novelData['nvl_name'] + '/';
 
-          novels.add(
-            Novel(
-              id: path,
-              title: title,
-              coverImageUrl: cover,
-              description: '',
-              genres: [],
-              chapters: [],
-              artist: '',
-              statusString: '',
-              pluginId: id,
-              author: '',
-            ),
-          );
-        }
+      for (final novelData in filteredNovels) {
+        final title = novelData['nvl_title'] as String;
+        final cover = '${apiURL}get-image/${novelData['image']}/novels/false';
+        final path = 'novelas/${novelData['id']}/${novelData['nvl_name']}/';
+
+        novels.add(
+          Novel(
+            id: path,
+            title: title,
+            coverImageUrl: cover,
+            description: '',
+            genres: [],
+            chapters: [],
+            artist: '',
+            statusString: '',
+            pluginId: id,
+            author: '',
+          ),
+        );
       }
     } else {
       print('Formato de resposta da API inesperado para searchNovels');
