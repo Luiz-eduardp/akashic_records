2df367da0496aafb0138b18154a092b08acebf1c
Eduardo <luizzeduardp@gmail.com>
2025-04-10 10:16:31 -0300

    feat: add changelog screen
    - Created changelog screen
    - Fetched changelog from GitHub
    - Improved UI/UX
    - Added update notification
    - Implemented update download

--- lib/main.dart
+++ lib/main.dart
@@ -10,9 +10,9 @@ import 'package:provider/provider.dart';
 import 'package:akashic_records/state/app_state.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:http/http.dart' as http;
-import 'package:flutter_markdown/flutter_markdown.dart';
-import 'package:url_launcher/url_launcher.dart';
 import 'package:package_info_plus/package_info_plus.dart';
+
+import 'package:akashic_records/screens/changelog/initial_loading_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -103,10 +103,10 @@ class _MyAppState extends State<MyApp> {
 
         setState(() {});
       } else {
-        print('Erro ao buscar releases: ${response.statusCode}');
+        debugPrint('Erro ao buscar releases: ${response.statusCode}');
       }
     } catch (e) {
-      print('Erro ao verificar versão: $e');
+      debugPrint('Erro ao verificar versão: $e');
     }
   }
 
@@ -189,206 +189,6 @@ class _MyAppState extends State<MyApp> {
             (context) => SettingsScreen(onLocaleChanged: _updateLocale),
         '/plugins': (context) => const PluginsScreen(),
       },
-    );
-  }
-}
-
-class InitialLoadingScreen extends StatefulWidget {
-  const InitialLoadingScreen({
-    super.key,
-    this.updateAvailable = false,
-    this.downloadUrl,
-  });
-
-  final bool updateAvailable;
-  final String? downloadUrl;
-
-  @override
-  State<InitialLoadingScreen> createState() => _InitialLoadingScreenState();
-}
-
-class _InitialLoadingScreenState extends State<InitialLoadingScreen> {
-  String _body = 'Carregando...'.translate;
-  String _uploader = 'Carregando...'.translate;
-  String _avatarUrl = '';
-  bool _isLoading = true;
-
-  @override
-  void initState() {
-    super.initState();
-    _fetchData();
-  }
-
-  Future<void> _fetchData() async {
-    try {
-      final response = await http.get(
-        Uri.parse(
-          'https://api.github.com/repos/AkashicRecordsApp/akashic_records/releases/latest',
-        ),
-      );
-
-      if (response.statusCode == 200) {
-        final data = json.decode(response.body);
-        final body = data['body'] as String;
-        final author = data['author'];
-        final uploaderLogin = author['login'] as String;
-        final avatarUrl = author['avatar_url'] as String;
-
-        setState(() {
-          _body = body;
-          _uploader = uploaderLogin;
-          _avatarUrl = avatarUrl;
-          _isLoading = false;
-        });
-      } else {
-        setState(() {
-          _body = 'Erro ao carregar'.translate;
-          _uploader = 'Erro ao carregar'.translate;
-          _isLoading = false;
-        });
-        print('Erro ao buscar releases: ${response.statusCode}');
-      }
-    } catch (e) {
-      setState(() {
-        _body = 'Erro ao carregar'.translate;
-        _uploader = 'Erro ao carregar'.translate;
-        _isLoading = false;
-      });
-      print('Erro: $e');
-    }
-  }
-
-  Future<void> _setInitialScreenShown() async {
-    final prefs = await SharedPreferences.getInstance();
-    await prefs.setBool('hasShownInitialScreen', true);
-  }
-
-  Future<void> _downloadAndInstall() async {
-    if (widget.downloadUrl != null) {
-      final Uri url = Uri.parse(widget.downloadUrl!);
-      if (!await launchUrl(url, mode: LaunchMode.externalApplication)) {
-        throw Exception('Could not launch ${widget.downloadUrl}');
-      }
-    }
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    final theme = Theme.of(context);
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'Changelog da Versão'.translate,
-          style: TextStyle(color: theme.colorScheme.onSurface),
-        ),
-        centerTitle: true,
-        backgroundColor: theme.colorScheme.surfaceVariant,
-        foregroundColor: theme.colorScheme.onSurfaceVariant,
-        surfaceTintColor: theme.colorScheme.surfaceVariant,
-      ),
-      body:
-          _isLoading
-              ? Center(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    CircularProgressIndicator(
-                      valueColor: AlwaysStoppedAnimation<Color>(
-                        theme.colorScheme.primary,
-                      ),
-                    ),
-                    const SizedBox(height: 16),
-                    Text(
-                      'Carregando dados do GitHub...'.translate,
-                      style: TextStyle(
-                        color: theme.colorScheme.onSurfaceVariant,
-                      ),
-                    ),
-                  ],
-                ),
-              )
-              : Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 50),
-                      MarkdownBody(
-                        data: _body,
-                        onTapLink: (text, url, title) {
-                          if (url != null) {
-                            launchUrl(Uri.parse(url));
-                          }
-                        },
-                        styleSheet: MarkdownStyleSheet.fromTheme(theme),
-                      ),
-                      const SizedBox(height: 24),
-                      Text(
-                        'Enviado por:'.translate,
-                        style: theme.textTheme.titleMedium?.copyWith(
-                          color: theme.colorScheme.onSurface,
-                        ),
-                      ),
-                      const SizedBox(height: 8),
-                      Row(
-                        children: [
-                          CircleAvatar(
-                            backgroundImage: NetworkImage(_avatarUrl),
-                          ),
-                          const SizedBox(width: 8),
-                          Text(
-                            _uploader,
-                            style: TextStyle(
-                              color: theme.colorScheme.onSurfaceVariant,
-                            ),
-                          ),
-                        ],
-                      ),
-                      if (widget.updateAvailable)
-                        Padding(
-                          padding: const EdgeInsets.only(top: 16.0),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              _downloadAndInstall();
-                            },
-                            style: ElevatedButton.styleFrom(
-                              backgroundColor: theme.colorScheme.primary,
-                              foregroundColor: theme.colorScheme.onPrimary,
-                              padding: const EdgeInsets.symmetric(
-                                horizontal: 24,
-                                vertical: 12,
-                              ),
-                              textStyle: const TextStyle(fontSize: 16),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(8),
-                              ),
-                            ),
-                            child: Text('Atualizar Aplicativo'.translate),
-                          ),
-                        ),
-                      const SizedBox(height: 32),
-                      Center(
-                        child: ElevatedButton(
-                          onPressed: () {
-                            _setInitialScreenShown();
-                            Navigator.of(context).pushReplacement(
-                              MaterialPageRoute(
-                                builder: (context) => const HomeScreen(),
-                              ),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: theme.colorScheme.primary,
-                            foregroundColor: theme.colorScheme.onPrimary,
-                          ),
-                          child: Text('Continuar'.translate),
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
     );
   }
 }
