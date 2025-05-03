cb6002704d9e83893ddc34057367c0a70499fe46
Eduardo <luizzeduardp@gmail.com>
2025-05-02 23:12:50 -0300

    build: Add Firebase Analytics
    - Added Firebase to the project
    - Updated dependencies
    - Configured Android and iOS
    - Added necessary files to .gitignore
    - Updated build files

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,8 @@
 import FlutterMacOS
 import Foundation
 
+import firebase_analytics
+import firebase_core
 import package_info_plus
 import path_provider_foundation
 import shared_preferences_foundation
@@ -13,6 +15,8 @@ import url_launcher_macos
 import webview_flutter_wkwebview
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
   FPPPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FPPPackageInfoPlusPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
