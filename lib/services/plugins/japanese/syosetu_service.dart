dd86ec5e085dc162eff31e3486eb4aa3b774c9da
Eduardo <luizzeduardp@gmail.com>
2025-05-03 22:52:45 -0300

    feat(plugins): add Syosetu plugin
    - Added Syosetu plugin for Japanese novels
    - Implemented search, popular novels, and
      novel parsing functionalities
    - Included chapter parsing and fetching
    - Added support for various filters
    - Uses http and html packages for data
      retrieval and parsing

---
+++ lib/services/plugins/japanese/syosetu_service.dart
@@ -0,0 +1,329 @@
+import 'package:akashic_records/models/model.dart';
+import 'package:akashic_records/models/plugin_service.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:http/http.dart' as http;
+import 'package:html/parser.dart' show parse;
+import 'package:html/dom.dart' as dom;
+
+class Syosetu implements PluginService {
+  @override
+  String get name => 'Syosetu';
+
+  @override
+  String get lang => 'ja';
+
+  @override
+  Map<String, dynamic> get filters => {
+    'ranking': {
+      'type': 'picker',
+      'label': 'Ranked by',
+      'options': [
+        {'label': '日間', 'value': 'daily'},
+        {'label': '週間', 'value': 'weekly'},
+        {'label': '月間', 'value': 'monthly'},
+        {'label': '四半期', 'value': 'quarter'},
+        {'label': '年間', 'value': 'yearly'},
+        {'label': '累計', 'value': 'total'},
+      ],
+      'value': 'total',
+    },
+    'genre': {
+      'type': 'picker',
+      'label': 'Ranking Genre',
+      'options': [
+        {'label': '総ジャンル', 'value': ''},
+        {'label': '異世界転生/転移〔恋愛〕〕', 'value': '1'},
+        {'label': '異世界転生/転移〔ファンタジー〕', 'value': '2'},
+        {'label': '異世界転生/転移〔文芸・SF・その他〕', 'value': 'o'},
+        {'label': '異世界〔恋愛〕', 'value': '101'},
+        {'label': '現実世界〔恋愛〕', 'value': '102'},
+        {'label': 'ハイファンタジー〔ファンタジー〕', 'value': '201'},
+        {'label': 'ローファンタジー〔ファンタジー〕', 'value': '202'},
+        {'label': '純文学〔文芸〕', 'value': '301'},
+        {'label': 'ヒューマンドラマ〔文芸〕', 'value': '302'},
+        {'label': '歴史〔文芸〕', 'value': '303'},
+        {'label': '推理〔文芸〕', 'value': '304'},
+        {'label': 'ホラー〔文芸〕', 'value': '305'},
+        {'label': 'アクション〔文芸〕', 'value': '306'},
+        {'label': 'コメディー〔文芸〕', 'value': '307'},
+        {'label': 'VRゲーム〔SF〕', 'value': '401'},
+        {'label': '宇宙〔SF〕', 'value': '402'},
+        {'label': '空想科学〔SF〕', 'value': '403'},
+        {'label': 'パニック〔SF〕', 'value': '404'},
+        {'label': '童話〔その他〕', 'value': '9901'},
+        {'label': '詩〔その他〕', 'value': '9902'},
+        {'label': 'エッセイ〔その他〕', 'value': '9903'},
+        {'label': 'その他〔その他〕', 'value': '9999'},
+      ],
+      'value': '',
+    },
+    'modifier': {
+      'type': 'picker',
+      'label': 'Modifier',
+      'options': [
+        {'label': 'すべて', 'value': 'total'},
+        {'label': '連載中', 'value': 'r'},
+        {'label': '完結済', 'value': 'er'},
+        {'label': '短編', 'value': 't'},
+      ],
+      'value': 'total',
+    },
+  };
+
+  final String id = 'Syosetu';
+  final String icon = 'src/jp/syosetu/icon.png';
+  final String site = 'https://yomou.syosetu.com/';
+  final String novelPrefix = 'https://ncode.syosetu.com';
+  @override
+  final String version = '1.1.2';
+  final Map<String, String> headers = {
+    "User-Agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+  };
+
+  String searchUrl(String? page, String? order) {
+    return '$site/search.php?order=${order ?? "hyoka"}${page != null ? "&p=${(int.tryParse(page) ?? 1) <= 1 || (int.tryParse(page) ?? 101) > 100 ? "1" : page}" : ""}';
+  }
+
+  static const String defaultCover =
+      'https://placehold.co/400x500.png?text=Sem+Capa';
+
+  Future<String> _fetchApi(String url, {Map<String, String>? headers}) async {
+    final response = await http.get(Uri.parse(url), headers: headers);
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      throw Exception('Falha ao carregar dados de: $url');
+    }
+  }
+
+  String _shrinkURL(String url) {
+    return url.replaceAll(RegExp(r'^.+ncode\.syosetu\.com'), '');
+  }
+
+  String _expandURL(String path) {
+    return novelPrefix + path;
+  }
+
+  @override
+  Future<List<Novel>> popularNovels(
+    int pageNo, {
+    Map<String, dynamic>? filters,
+  }) async {
+    final String ranking = filters?['ranking']?['value'] ?? 'total';
+    final String genre = filters?['genre']?['value'] ?? '';
+    final String modifier = filters?['modifier']?['value'] ?? 'total';
+    final String page = pageNo.toString();
+
+    String url;
+    if (genre.isNotEmpty) {
+      url =
+          '$site/rank/${genre.length == 1 ? "isekailist" : "genrelist"}/type/${ranking}_$genre${modifier == "total" ? "" : "_$modifier"}?p=$page';
+    } else {
+      url = '$site/rank/list/type/${ranking}_$modifier/?p=$page';
+    }
+
+    print("Popular URL: $url");
+
+    final body = await _fetchApi(url, headers: headers);
+    final document = parse(body);
+
+    List<Novel> novels = [];
+
+    final novelElements = document.querySelectorAll('.c-card');
+
+    for (var element in novelElements) {
+      final anchor = element.querySelector('.p-ranklist-item__title a');
+      final path = anchor?.attributes['href'];
+
+      if (path != null) {
+        final name = anchor?.text.trim();
+
+        final novel = Novel(
+          id: _shrinkURL(path.replaceFirst(novelPrefix, '')),
+          title: name ?? 'Unknown Title',
+          coverImageUrl: defaultCover,
+          pluginId: this.name,
+          author: '',
+          description: '',
+          genres: [],
+          chapters: [],
+          artist: '',
+          statusString: '',
+        );
+        novels.add(novel);
+      }
+    }
+
+    return novels;
+  }
+
+  Future<List<Chapter>> parseChaptersFromPage(dom.Document document) async {
+    List<Chapter> chapters = [];
+    document.querySelectorAll('.p-eplist__sublist').forEach((element) {
+      final anchor = element.querySelector('a');
+      final href = anchor?.attributes['href'];
+      final name = anchor?.text.trim() ?? '';
+      final releaseTimeText = element
+          .querySelector('.p-eplist__update')
+          ?.text
+          .trim()
+          .split(" ")[0]
+          .replaceAll('/', '-');
+      final releaseTime = releaseTimeText ?? '';
+
+      if (href != null) {
+        chapters.add(
+          Chapter(
+            id: _shrinkURL(href.replaceFirst(novelPrefix, '')),
+            title: name,
+            content: '',
+            chapterNumber: chapters.length + 1,
+            releaseDate: releaseTime,
+          ),
+        );
+      }
+    });
+    return chapters;
+  }
+
+  @override
+  Future<Novel> parseNovel(String novelPath) async {
+    final url = _expandURL(novelPath);
+    final body = await _fetchApi(url, headers: headers);
+    final document = parse(body);
+
+    String title =
+        document.querySelector('.p-novel__title')?.text.trim() ?? 'Sem título';
+    String author =
+        document
+            .querySelector('.p-novel__author')
+            ?.text
+            .replaceAll('作者：', '')
+            .trim() ??
+        'Sem Autor';
+    String description =
+        document.querySelector('#novel_ex')?.innerHtml ?? 'Sem Descrição';
+    final descriptionTag =
+        document
+            .querySelector('meta[property="og:description"]')
+            ?.attributes['content'];
+    String genresString = descriptionTag ?? '';
+    List<String> genres = genresString.split(" ").map((e) => e.trim()).toList();
+
+    NovelStatus status = NovelStatus.Desconhecido;
+
+    String statusString =
+        document.querySelector('.c-announce')?.text.trim() ?? '';
+    if (statusString.contains("連載中") || statusString.contains("未完結")) {
+      status = NovelStatus.Andamento;
+    } else if (statusString.contains("完結")) {
+      status = NovelStatus.Completa;
+    }
+
+    List<Chapter> chapters = [];
+
+    final chapterElements = document.querySelectorAll('.p-eplist__sublist');
+    for (var element in chapterElements) {
+      final anchor = element.querySelector('a');
+      final chapterUrl = anchor?.attributes['href'];
+      final chapterName = anchor?.text.trim() ?? '';
+      final chapterReleaseTime = element
+          .querySelector('.p-eplist__update')
+          ?.text
+          .trim()
+          .split(" ")[0]
+          .replaceAll('/', '-');
+
+      if (chapterUrl != null) {
+        final chapter = Chapter(
+          id: _shrinkURL(chapterUrl.replaceFirst(novelPrefix, '')),
+          title: chapterName,
+          content: '',
+          chapterNumber: chapters.length + 1,
+          releaseDate: chapterReleaseTime,
+        );
+        chapters.add(chapter);
+      }
+    }
+
+    final novel = Novel(
+      id: novelPath,
+      title: title,
+      coverImageUrl: defaultCover,
+      description: description,
+      genres: genres,
+      artist: '',
+      status: status,
+      statusString: '',
+      author: author,
+      pluginId: name,
+      chapters: chapters,
+    );
+
+    return novel;
+  }
+
+  @override
+  Future<String> parseChapter(String chapterPath) async {
+    final url = _expandURL(chapterPath);
+    final body = await _fetchApi(url, headers: headers);
+    final document = parse(body);
+
+    final chapterTitle =
+        document.querySelector('.p-novel__title')?.innerHtml ?? '';
+    final chapterText =
+        document
+            .querySelector(
+              '.p-novel__body .p-novel__text:not([class*="p-novel__text--"])',
+            )
+            ?.innerHtml ??
+        '';
+
+    return "<h1>$chapterTitle</h1>$chapterText";
+  }
+
+  @override
+  Future<List<Novel>> searchNovels(
+    String searchTerm,
+    int pageNo, {
+    Map<String, dynamic>? filters,
+  }) async {
+    String url = searchUrl(pageNo.toString(), null) + "&word=" + searchTerm;
+
+    final body = await _fetchApi(url, headers: headers);
+    final document = parse(body);
+
+    List<Novel> novels = [];
+
+    final novelElements = document.querySelectorAll(".searchkekka_box");
+
+    for (var element in novelElements) {
+      final titleAnchor = element.querySelector(".novel_h a");
+      final novelPath = titleAnchor?.attributes['href'];
+      final novelName = titleAnchor?.text.trim() ?? '';
+
+      if (novelPath != null) {
+        final novel = Novel(
+          id: _shrinkURL(novelPath.replaceFirst(novelPrefix, '')),
+          title: novelName,
+          coverImageUrl: defaultCover,
+          pluginId: name,
+          author: '',
+          description: '',
+          genres: [],
+          chapters: [],
+          artist: '',
+          statusString: '',
+        );
+        novels.add(novel);
+      }
+    }
+    return novels;
+  }
+
+  @override
+  Future<List<Novel>> getAllNovels({BuildContext? context}) {
+    return Future.value([]);
+  }
+}
--- lib/state/app_state.dart
+++ lib/state/app_state.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 import 'package:akashic_records/models/plugin_service.dart';
 import 'package:akashic_records/screens/settings/appearance_settings.dart';
 import 'package:akashic_records/services/plugins/japanese/kakuyomu_service.dart';
+import 'package:akashic_records/services/plugins/japanese/syosetu_service.dart';
 import 'package:akashic_records/services/plugins/spanish/novelsligera_service.dart';
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -24,13 +25,8 @@ import 'package:akashic_records/services/plugins/portuguese/centralnovel_service
 import 'package:akashic_records/models/favorite_list.dart';
 import 'package:akashic_records/models/model.dart';
 import 'package:akashic_records/i18n/i18n.dart';
-enum PluginLanguage {
-  ptBr,
-  en,
-  es,
-  ja,
-  // Adicione mais idiomas conforme necessário
-}
+
+enum PluginLanguage { ptBr, en, es, ja }
 
 class PluginInfo {
   final String name;
@@ -38,6 +34,7 @@ class PluginInfo {
 
   PluginInfo({required this.name, required this.language});
 }
+
 enum ReaderTheme {
   Akashic,
   light,
@@ -313,7 +310,7 @@ class AppState with ChangeNotifier {
   List<FavoriteList> _favoriteLists = [];
   int _novelCount = 0;
 
- final Map<String, PluginService> _pluginServices = {};
+  final Map<String, PluginService> _pluginServices = {};
   final Map<String, PluginInfo> _pluginInfo = {};
   final _uuid = const Uuid();
   static const String _novelCachePrefix = 'novel_cache_';
@@ -331,50 +328,93 @@ class AppState with ChangeNotifier {
 
   AppState() {
     _pluginServices['NovelMania'] = NovelMania();
-    _pluginInfo['NovelMania'] = PluginInfo(name: 'NovelMania', language: PluginLanguage.ptBr);
+    _pluginInfo['NovelMania'] = PluginInfo(
+      name: 'NovelMania',
+      language: PluginLanguage.ptBr,
+    );
 
     _pluginServices['Tsundoku'] = Tsundoku();
-    _pluginInfo['Tsundoku'] = PluginInfo(name: 'Tsundoku', language: PluginLanguage.ptBr);
+    _pluginInfo['Tsundoku'] = PluginInfo(
+      name: 'Tsundoku',
+      language: PluginLanguage.ptBr,
+    );
 
     _pluginServices['CentralNovel'] = CentralNovel();
-    _pluginInfo['CentralNovel'] = PluginInfo(name: 'CentralNovel', language: PluginLanguage.ptBr);
+    _pluginInfo['CentralNovel'] = PluginInfo(
+      name: 'CentralNovel',
+      language: PluginLanguage.ptBr,
+    );
 
     _pluginServices['MtlNovelPt'] = MtlNovelPt();
-    _pluginInfo['MtlNovelPt'] = PluginInfo(name: 'MtlNovelPt', language: PluginLanguage.ptBr);
+    _pluginInfo['MtlNovelPt'] = PluginInfo(
+      name: 'MtlNovelPt',
+      language: PluginLanguage.ptBr,
+    );
 
     _pluginServices['LightNovelBrasil'] = LightNovelBrasil();
-    _pluginInfo['LightNovelBrasil'] = PluginInfo(name: 'LightNovelBrasil', language: PluginLanguage.ptBr);
+    _pluginInfo['LightNovelBrasil'] = PluginInfo(
+      name: 'LightNovelBrasil',
+      language: PluginLanguage.ptBr,
+    );
 
     _pluginServices['BlogDoAmonNovels'] = BlogDoAmonNovels();
-    _pluginInfo['BlogDoAmonNovels'] = PluginInfo(name: 'BlogDoAmonNovels', language: PluginLanguage.ptBr);
+    _pluginInfo['BlogDoAmonNovels'] = PluginInfo(
+      name: 'BlogDoAmonNovels',
+      language: PluginLanguage.ptBr,
+    );
 
-    // Inglês
     _pluginServices['NovelsOnline'] = NovelsOnline();
-    _pluginInfo['NovelsOnline'] = PluginInfo(name: 'NovelsOnline', language: PluginLanguage.en);
+    _pluginInfo['NovelsOnline'] = PluginInfo(
+      name: 'NovelsOnline',
+      language: PluginLanguage.en,
+    );
 
     _pluginServices['RoyalRoad'] = RoyalRoad();
-    _pluginInfo['RoyalRoad'] = PluginInfo(name: 'RoyalRoad', language: PluginLanguage.en);
+    _pluginInfo['RoyalRoad'] = PluginInfo(
+      name: 'RoyalRoad',
+      language: PluginLanguage.en,
+    );
 
     _pluginServices['Webnovel'] = Webnovel();
-    _pluginInfo['Webnovel'] = PluginInfo(name: 'Webnovel', language: PluginLanguage.en);
+    _pluginInfo['Webnovel'] = PluginInfo(
+      name: 'Webnovel',
+      language: PluginLanguage.en,
+    );
 
     _pluginServices['ReaperScans'] = ReaperScans();
-    _pluginInfo['ReaperScans'] = PluginInfo(name: 'ReaperScans', language: PluginLanguage.en);
+    _pluginInfo['ReaperScans'] = PluginInfo(
+      name: 'ReaperScans',
+      language: PluginLanguage.en,
+    );
 
     _pluginServices['NovelBin'] = NovelBin();
-    _pluginInfo['NovelBin'] = PluginInfo(name: 'NovelBin', language: PluginLanguage.en);
+    _pluginInfo['NovelBin'] = PluginInfo(
+      name: 'NovelBin',
+      language: PluginLanguage.en,
+    );
 
-    // Espanhol
     _pluginServices['SkyNovels'] = SkyNovels();
-    _pluginInfo['SkyNovels'] = PluginInfo(name: 'SkyNovels', language: PluginLanguage.es);
+    _pluginInfo['SkyNovels'] = PluginInfo(
+      name: 'SkyNovels',
+      language: PluginLanguage.es,
+    );
 
     _pluginServices['NovelasLigera'] = NovelasLigera();
-    _pluginInfo['NovelasLigera'] = PluginInfo(name: 'NovelasLigera', language: PluginLanguage.es);
+    _pluginInfo['NovelasLigera'] = PluginInfo(
+      name: 'NovelasLigera',
+      language: PluginLanguage.es,
+    );
 
-    // Japonês
     _pluginServices['Kakuyomu'] = Kakuyomu();
-    _pluginInfo['Kakuyomu']  = PluginInfo(name: 'Kakuyomu', language: PluginLanguage.ja);
-
+    _pluginInfo['Kakuyomu'] = PluginInfo(
+      name: 'Kakuyomu',
+      language: PluginLanguage.ja,
+    );
+    _pluginServices['Syosetu'] = Syosetu();
+    _pluginInfo['Syosetu'] = PluginInfo(
+      name: 'Syosetu',
+      language: PluginLanguage.ja,
+    );
   }
 
   ThemeMode get themeMode => _themeMode;
