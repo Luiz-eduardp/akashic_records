3056522f1aef690ff99932e4efbf72f48e5501f9
Eduardo <luizzeduardp@gmail.com>
2025-09-30 12:16:38 -0300

    fix: corrigir a formatação do código para manter a consistência

--- lib/services/plugins/portuguese/centralnovel_service.dart
+++ lib/services/plugins/portuguese/centralnovel_service.dart
@@ -22,7 +22,7 @@ class CentralNovel implements PluginService {
   @override
   Map<String, dynamic> get filters => {};
   @override
-  String get siteUrl => baseURL; 
+  String get siteUrl => baseURL;
   final String id = 'CentralNovel';
   final String nameService = 'Central Novel';
   final String baseURL = 'https://centralnovel.com';
@@ -98,8 +98,8 @@ class CentralNovel implements PluginService {
   @override
   Future<List<Novel>> popularNovels(
     int pageNo, {
-    Map<String, dynamic>? filters,  
-      BuildContext? context,
+    Map<String, dynamic>? filters,
+    BuildContext? context,
   }) async {
     final url = 'https://centralnovel.com/series/?order=popular&page=$pageNo';
     return await _parseList(url);
