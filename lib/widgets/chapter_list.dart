32cda9e7993f620c0e9c74f85054049620d453e2
Eduardo <luizzeduardp@gmail.com>
2025-09-30 12:15:21 -0300

    feat: create chapter list widget
    - Display list of chapters
    - Mark read chapters
    - Handle chapter tap event

---
+++ lib/widgets/chapter_list.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:akashic_records/models/model.dart';
+
+typedef ChapterTap = void Function(Chapter chapter, int index);
+
+class ChapterList extends StatefulWidget {
+  final List<Chapter> chapters;
+  final Set<String> readChapters;
+  final ChapterTap onTap;
+
+  const ChapterList({
+    super.key,
+    required this.chapters,
+    required this.readChapters,
+    required this.onTap,
+  });
+
+  @override
+  State<ChapterList> createState() => _ChapterListState();
+}
+
+class _ChapterListState extends State<ChapterList> {
+  @override
+  Widget build(BuildContext context) {
+    return ListView.separated(
+      itemCount: widget.chapters.length,
+      separatorBuilder: (_, __) => const Divider(height: 1),
+      itemBuilder: (ctx, i) {
+        final ch = widget.chapters[i];
+        final isRead = widget.readChapters.contains(ch.id);
+        return ListTile(
+          title: Text(ch.title, maxLines: 2, overflow: TextOverflow.ellipsis),
+          subtitle: ch.releaseDate != null ? Text(ch.releaseDate!) : null,
+          trailing:
+              isRead
+                  ? const Icon(Icons.check_circle, color: Colors.green)
+                  : null,
+          onTap: () => widget.onTap(ch, i),
+        );
+      },
+    );
+  }
+}
