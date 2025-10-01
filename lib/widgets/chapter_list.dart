3c87cfe8e2357f389df3d36354d9550ecc2f6077
Eduardo <luizzeduardp@gmail.com>
2025-10-01 11:09:16 -0300

    refactor: Simplify chapter list sorting

--- lib/widgets/chapter_list.dart
+++ lib/widgets/chapter_list.dart
@@ -25,39 +25,12 @@ class ChapterList extends StatefulWidget {
 class _ChapterListState extends State<ChapterList> {
   @override
   Widget build(BuildContext context) {
-    final originalIndices = List<int>.generate(
-      widget.chapters.length,
-      (i) => i,
-    );
-    final paired = List.generate(
-      widget.chapters.length,
-      (i) => MapEntry(widget.chapters[i], originalIndices[i]),
-    );
-    paired.sort((a, b) {
-      final Chapter ca = a.key;
-      final Chapter cb = b.key;
-      if ((ca.releaseDate?.isNotEmpty ?? false) &&
-          (cb.releaseDate?.isNotEmpty ?? false)) {
-        try {
-          final da = DateTime.tryParse(ca.releaseDate!);
-          final db = DateTime.tryParse(cb.releaseDate!);
-          if (da != null && db != null) return db.compareTo(da);
-        } catch (_) {}
-        return cb.releaseDate!.compareTo(ca.releaseDate!);
-      }
-
-      if ((ca.chapterNumber != null) && (cb.chapterNumber != null)) {
-        return (cb.chapterNumber ?? 0).compareTo(ca.chapterNumber ?? 0);
-      }
-
-      return b.value.compareTo(a.value);
-    });
     return ListView.separated(
-      itemCount: paired.length,
+      itemCount: widget.chapters.length,
       separatorBuilder: (_, __) => const Divider(height: 1),
       itemBuilder: (ctx, i) {
-        final ch = paired[i].key;
-        final originalIndex = paired[i].value;
+        final ch = widget.chapters[i];
+        final originalIndex = i;
         final isRead = widget.readChapters.contains(ch.id);
         return ListTile(
           title: Text(ch.title, maxLines: 2, overflow: TextOverflow.ellipsis),
